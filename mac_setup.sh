ca4484d3fe930becb707ecbc5ef908f515f226b4
xima <xima@ebay.com>
2018-12-21 15:40:22 -0800

    first commit

---
+++ mac_setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# macbook set up - xinma
+# copy vim profile
+
+# install or update xcode
+xcode-select --install
+
+# set up tools.
+brew update
+brew install git telnet fping
+sudo gem install iStats
+
+# set up ssh keys
+ssh-keygen -t rsa -b 4096 -C "xima@ebay.com"
+# adding ssh public key to github, internal github and gitlab
+
+# set up alias
+
+# set up kubes
+
+# set up minikube
+
+# set up vagrant and virtualbox
+
+# set up IDE
